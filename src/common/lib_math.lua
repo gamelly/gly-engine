9f041dc20e3dc6a17bde53610b42cc194f6dd09c
RodrigoDornelles <rodrigo@dornelles.me>
2024-06-26 18:09:54 -0300

    feat: add math funcs lerp, cycle, saw, map

--- src/common/lib_math.lua
+++ src/common/lib_math.lua
@@ -15,9 +15,37 @@ local function abs(value)
     return value
 end
 
+local function saw(x)
+    if x < 0.25 then
+        return x * 4
+    elseif x < 0.50 then
+        return 1 - ((x - 0.25) * 4)
+    elseif x < 0.75 then
+        return ((x - 0.50) * 4) * (-1)
+    end
+    return ((x - 0.75) * 4) - 1
+end
+
+local function lerp(a, b, alpha)
+    return a + alpha * ( b - a )
+end 
+
+local function map(x, in_min, in_max, out_min, out_max)
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+end
+
+local function cycle(passed, duration)
+    local endtime = (passed) % duration
+    return ((endtime == 0 and (passed % (duration * 2)) or endtime)) / duration
+end
+
 local P = {
+    cycle=cycle,
     clamp=clamp,
-    abs=abs
+    lerp=lerp,
+    abs=abs,
+    map=map,
+    saw=saw
 }
 
 return P;
