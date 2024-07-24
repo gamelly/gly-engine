b01943f7bd66ac09756880eef3245aa8df9e90d2
Diego Ferreira <diegofs01@hotmail.com>
2024-07-24 13:24:26 -0300

    basic bash argument

--- broco.sh
+++ broco.sh
@@ -1,1 +1,28 @@
-./cli.sh build ./broco/game.lua --core love --run
+if [ -z "$1" ]; then
+    echo "error: no argument"
+    echo "options available:"
+    echo "love, html5"
+    exit 1
+fi
+
+case $1 in
+
+    love | lua)
+        ./cli.sh build ./broco/game.lua --core love --run
+    ;;
+
+    html | html5)
+        ./cli.sh build ./broco/game.lua --core html5
+    ;;
+
+    ginga)
+        # ./cli.sh build ./broco/game.lua --core love --run
+    ;;
+
+    *)
+        echo "error: invalid argument"
+    ;;
+
+esac
+
+# ./cli.sh build ./broco/game.lua --core love --run
