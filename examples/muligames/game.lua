296612a1ee4a0e4857d0085afdbcd61694aa9b8c
RodrigoDornelles <rodrigo@dornelles.me>
2024-07-25 15:44:49 -0300

    feat: example multigames loader

---
+++ examples/muligames/game.lua
@@ -0,0 +1,90 @@
+local function init(std, game)
+    local registry = 'gist.githubusercontent.com/RodrigoDornelles/7ee1ac926b76442d6c303c3a291037c4/raw/c3597529459e487eadccd8597fb1dc72e5034096/games.csv'
+    game.multi_text = 'booting...'
+    game.multi_state = 1
+    game.multi_menu = 1
+    game.multi_menu_time = game.milis
+    game.multi_list = {}
+    std.http.get(registry):run()
+end
+
+local function http(std, game)
+    if std.http.ok and game.multi_state == 1 then
+        std.csv(std.http.body, game.multi_list)
+        game.multi_state = 2
+    elseif std.http.ok and game.multi_state == 3 then 
+        game.multi_state = 1
+        game.application = load(std.http.body)
+        game.application = game.application and game.application()
+        if game.application then
+            game.multi_state = 4
+            game.application.callbacks.init(std, game)
+        end
+    else
+        game.multi_state = 1
+        game.multi_text = std.http.body or std.http.error
+    end
+end
+
+local function loop(std, game)
+    if game.multi_state == 2 then
+        local key = std.key.press.down - std.key.press.up
+        if key ~= 0 and game.milis > game.multi_menu_time + 250 then
+            game.multi_menu = std.math.clamp2(game.multi_menu + key, 1, 2)
+            game.multi_menu_time = game.milis
+        end
+        if std.key.press.enter == 1 and game.milis > game.multi_menu_time + 250 then
+            game.multi_state = 3
+            game.multi_menu_time = game.milis
+            std.http.get(game.multi_list[game.multi_menu].raw_url):run()
+        end
+    elseif game.multi_state == 4 then
+        game.application.callbacks.loop(std, game)
+    end
+end
+
+local function draw(std, game)
+    if game.multi_state == 1 then
+        std.draw.clear(std.color.red)
+        std.draw.color(std.color.white)
+        std.draw.text(8, 8, game.multi_text)
+    elseif game.multi_state == 3 then
+        std.draw.clear(std.color.blue)
+        std.draw.color(std.color.white)
+        std.draw.text(8, 8, 'loading...')
+    elseif game.multi_state == 2 then
+        local s = 0
+        std.draw.clear(std.color.darkgray)
+        std.draw.color(std.color.white)
+        local index = 1
+        while index <= #game.multi_list do
+            std.draw.text(16, 8 + (index * 14), game.multi_list[index].title)
+            std.draw.text(100, 8 + (index * 14), game.multi_list[index].version)
+            s=std.math.max(std.draw.text(200, 8 + (index * 12), game.multi_list[index].author), s)
+            index = index + 1
+        end
+        std.draw.color(std.color.red)
+        std.draw.rect(1, 16, 5 + (game.multi_menu * 16), 200 + s, 14)
+    elseif game.multi_state == 4 then
+        game.application.callbacks.draw(std, game)
+    end
+end
+
+local P = {
+    meta={
+        title='Multi Games',
+        author='Rodrigo Dornelles',
+        version='1.0.0'
+    },
+    config={
+        require='http random math csv'
+    },
+    callbacks={
+        init=init,
+        loop=loop,
+        draw=draw,
+        http=http
+    }
+}
+
+return P
---
+++ src/lib/engine/csv.lua
@@ -0,0 +1,47 @@
+local function csv(in_str, out_table)
+    local index1 = 1
+    local headers = {}
+    local pattern = '[^,]+'
+    local next_line = in_str:gmatch('([^\r\n]*)\r?\n?')
+    local line = next_line()
+    local next_header = line:gmatch(pattern)
+
+    repeat
+        local header = next_header()
+        headers[index1] = header
+        index1 = index1 + 1
+    until not header
+
+    local index2 = 1
+    repeat
+        line = next_line()
+        if line then
+            local next_value = line:gmatch(pattern)
+            index1 = 1
+            repeat 
+                local value = next_value()
+                local header = headers[index1]
+                if header and value and not out_table[index2] then
+                    out_table[index2] = {}
+                end
+                if header and value then
+                    out_table[index2][header] = value
+                end
+                index1 = index1 + 1
+            until not value
+        end
+        index2 = index2 + 1
+    until not line
+end
+
+local function install(std)
+    std = std or {}
+    std.csv = csv
+    return std.csv
+end
+
+local P = {
+    install=install
+}
+
+return P
