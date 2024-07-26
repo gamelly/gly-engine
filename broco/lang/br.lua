43bccf3b740408773145ac9fd238284ca044882d
Diego Ferreira <diegofs01@hotmail.com>
2024-07-26 12:14:54 -0300

    add mock for multiple languages

---
+++ broco/lang/br.lua
@@ -0,0 +1,22 @@
+lang = "br"
+authors = {"diegofs01"}
+menu = {
+    novo_jogo = "novo jogo"
+    sair = "sair"
+    dificuldade = "dificuldade"
+    facil = "fácil"
+    medio = "médio"
+    dificil = "difícil"
+}
+pause = {
+    pausado = "pause",
+    continuar = "continuar",
+    menu = "menu",
+    sair = "sair",
+}
+board = {
+    pontos = "pontos",
+    pontos_alto = "pontos mais alto",
+    contador_broco = "",
+    selecionado = "selecionado",
+}
---
+++ broco/lang/en.lua
@@ -0,0 +1,22 @@
+lang = "en"
+authors = {"diegofs01"}
+menu = {
+    novo_jogo = "new game"
+    sair = "exit"
+    dificuldade = "difficulty"
+    facil = "easy"
+    medio = "medium"
+    dificil = "hard"
+}
+pause = {
+    pausado = "pause",
+    continuar = "continue",
+    menu = "menu",
+    sair = "exit",
+}
+board = {
+    pontos = "score",
+    pontos_alto = "high score",
+    contador_broco = "broco counter",
+    selecionado = "selected",
+}
