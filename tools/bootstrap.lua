268606112decfa6a4c2461231f782b0d66476575
RodrigoDornelles <rodrigo@dornelles.me>
2024-09-18 11:45:17 -0300

    feat: support multi path in bootstrap tool

--- tools/bootstrap.lua
+++ tools/bootstrap.lua
@@ -42,47 +42,67 @@ local function map_files(files, prefix_len)
     return list_files, dict_files
 end
 
+local function merge_dict_and_lists(list_out, dict_out, list_in, dict_in)
+    local index = 1
+    while index <= #list_in do
+        local key = list_in[index]
+        list_out[#list_out + 1] = key
+        dict_out[key] = dict_in[key]
+        index = index + 1
+    end
+end
+
 local function main(args)
     local input_file = io.open(args[1], 'r')
     local output_file = io.open(args[2], 'w')
-    local path_src = args[3] or './src'
-    local prefix_path = path_src:match("^(.-)/[^/]+$")
-    local prefix_len = 0
-    
-    if not prefix_path then
-        print('path_src must be a explicit relative path or absolute')
-        return 1
-    elseif path_src:sub(1, 2) == './' then
-        prefix_len = 3
-    else 
-        prefix_len = #prefix_path + 2
+
+    if #args <= 2 then
+        print('missing src\'s to bundle!')
     end
 
     if not input_file or not output_file then
-        print('usage: lua bootstrap.lua ./dist/main.lua ./dist/cli.lua ./src')
+        print('usage: lua bootstrap.lua ./dist/main.lua ./dist/cli.lua ./src ./assets')
         return 1
     end
 
-    do 
-        local f = io.open(path_src, 'rb')
-        if not f then 
-            print('directory not found')
+    local index = 3
+    local all_list_files = {}
+    local all_dict_files = {}
+    while index <= #args do
+        local path_src = args[index] or './src'
+        local prefix_path = path_src:match("^(.-)/[^/]+$")
+        local prefix_len = 0
+        
+        if not prefix_path then
+            print('src path must be a explicit relative path or absolute')
             return 1
+        elseif path_src:sub(1, 2) == './' then
+            prefix_len = 3
+        else 
+            prefix_len = #prefix_path + 2
         end
-        local can_read = f:read(1)
-        if can_read then
-            print('path_src must be a directory')
-            return 1
+
+        do 
+            local f = io.open(path_src, 'rb')
+            if not f then 
+                print('directory not found')
+                return 1
+            end
+            local can_read = f:read(1)
+            if can_read then
+                print('path src must be a directory')
+                return 1
+            end
+            f:close()
         end
-        f:close()
+
+        local cmd_pid = io.popen('find '..path_src)
+        local cmd_raw, cmd_pid = cmd_pid:read('*a'), cmd_pid:close()
+        local list_raw = explode_string(cmd_raw)
+        local list_files, dict_files = map_files(list_raw, prefix_len)
+        merge_dict_and_lists(all_list_files, all_dict_files, list_files, dict_files)
+        index = index + 1
     end
-
-    local index = 1
-    local cmd_pid = io.popen('find '..path_src)
-    local cmd_raw, cmd_pid = cmd_pid:read('*a'), cmd_pid:close()
-    local list_raw = explode_string(cmd_raw)
-    local list_files, dict_files = map_files(list_raw, prefix_len)
-
 
     output_file:write([[
 local real_io_open = io.open
@@ -94,7 +114,7 @@ io.open = function(filename, mode)
                 if self.pointer >= #_G[filename] then
                     return nil
                 elseif type(size) == 'number' then
-                    local content = _G[filename]:sub(self.pointer, size)
+                    local content = _G[filename]:sub(self.pointer, self.pointer + size)
                     self.pointer = self.pointer + #content
                     return content
                 elseif size == '*a' then
@@ -124,9 +144,10 @@ io.open = function(filename, mode)
 end
 ]])
 
-    while index <= #list_files do
-        local file_name = list_files[index]
-        local file_content = dict_files[file_name]
+    index = 1
+    while index <= #all_list_files do
+        local file_name = all_list_files[index]
+        local file_content = all_dict_files[file_name]
         output_file:write('_G[\''..file_name..'\'] = \''..file_content..'\'\n')
         index = index + 1
     end
