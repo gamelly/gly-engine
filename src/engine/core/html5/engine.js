d60407bd183b10683514a036a654a7343de4ebb2
RodrigoDornelles <rodrigo@dornelles.me>
2024-09-26 10:33:28 -0300

    change: wasmoon decoupling in core html5

--- src/engine/core/html5/engine.js
+++ src/engine/core/html5/engine.js
@@ -1,159 +1,203 @@
-import { LuaFactory, LuaMultiReturn }  from 'https://cdn.jsdelivr.net/npm/wasmoon@1.16.0/+esm'
-
-document.addEventListener('DOMContentLoaded', async () => {
-    const factory = new LuaFactory()
-    const lua = await factory.createEngine()
-
-    const engine_response = await fetch('./main.lua')
-    const game_response = await fetch('./game.lua')
-
-    const engine_lua = await engine_response.text()
-    const game_lua = await game_response.text()
-
-    const body_element = document.querySelector('body')
-    const canvas_element = document.querySelector('#gameCanvas')
-    const canvas_ctx = canvas_element.getContext("2d")
-    const canvas_close = [
-        () => canvas_ctx.fill(),
-        () => {
-            canvas_ctx.closePath()
-            canvas_ctx.stroke()
+const engine = {
+    stop: false,
+    error: {
+        callback: null,
+        capture: false,
+        console: false,
+        canvas: false,
+        stop: false   
+    },
+    listen: {},
+    global: {
+        native_draw_start: () => {
+            engine.canvas_ctx.clearRect(0, 0, engine.canvas_element.width, engine.canvas_element.height)
         },
-        () => canvas_ctx.stroke()
-    ]
-
-    lua.global.set('native_draw_start', () => {
-        canvas_ctx.clearRect(0, 0, canvas_element.width, canvas_element.height)
-    })
-    lua.global.set('native_draw_flush', () => {})
-    lua.global.set('native_draw_clear', (color) => {
-        canvas_ctx.fillStyle = '#' + color.toString(16).padStart(8, '0')
-        canvas_ctx.fillRect(0, 0, canvas_element.width, canvas_element.height)
-    })
-    lua.global.set('native_draw_color', (color) => {
-        const hex = '#' + color.toString(16).padStart(8, '0')
-        canvas_ctx.strokeStyle = hex
-        canvas_ctx.fillStyle = hex
-    })
-    lua.global.set('native_draw_line', (x1, y1, x2, y2) => {
-        canvas_ctx.beginPath()
-        canvas_ctx.moveTo(x1, y1)
-        canvas_ctx.lineTo(x2, y2)
-        canvas_ctx.stroke()
-    })
-    lua.global.set('native_draw_rect', (mode, x, y, w, h) => mode === 1?
-        canvas_ctx.strokeRect(x, y, w, h):
-        canvas_ctx.fillRect(x, y, w, h)
-    )
-    lua.global.set('native_draw_font', (name, size) => {})
-    lua.global.set('native_draw_text', (x, y, text) => {
-        x && y && canvas_ctx.fillText(text, x, y)
-        const { width, actualBoundingBoxAscent, actualBoundingBoxDescent } = canvas_ctx.measureText(text || x)
-        return LuaMultiReturn.from([width, actualBoundingBoxAscent + actualBoundingBoxDescent])
-    })
-    lua.global.set('native_dict_poly', {
-        poly2: (mode, verts, x, y, scale = 1, angle = 0, ox = 0, oy = 0) => {
-            let index = 0
-            canvas_ctx.beginPath()
-            while (index < verts.length) {
-                const px = verts[index];
-                const py = verts[index + 1];
-                const xx = x + ((ox - px) * -scale * Math.cos(angle)) - ((ox - py) * -scale * Math.sin(angle));
-                const yy = y + ((oy - px) * -scale * Math.sin(angle)) + ((oy - py) * -scale * Math.cos(angle));
-                if (index < 2) {
-                    canvas_ctx.moveTo(xx, yy)
-                } else {
-                    canvas_ctx.lineTo(xx, yy)
-                }
-                index = index + 2;
+        native_draw_flush: () => {},
+        native_draw_clear: (color) => {
+            engine.canvas_ctx.fillStyle = '#' + color.toString(16).padStart(8, '0')
+            engine.canvas_ctx.fillRect(0, 0, engine.canvas_element.width, engine.canvas_element.height)
+        },
+        native_draw_color: (color) => {
+            const hex = '#' + color.toString(16).padStart(8, '0')
+            engine.canvas_ctx.strokeStyle = hex
+            engine.canvas_ctx.fillStyle = hex
+        },
+        native_draw_line: (x1, y1, x2, y2) => {
+            engine.canvas_ctx.beginPath()
+            engine.canvas_ctx.moveTo(x1, y1)
+            engine.canvas_ctx.lineTo(x2, y2)
+            engine.canvas_ctx.stroke()
+        },
+        native_draw_rect: (mode, x, y, w, h) => {
+            mode === 1 ? engine.canvas_ctx.strokeRect(x, y, w, h) : engine.canvas_ctx.fillRect(x, y, w, h)
+        },
+        native_draw_font: (name, size) => {},
+        native_draw_text: (x, y, text) => {
+            if (x && y) {
+                engine.canvas_ctx.fillText(text, x, y)
             }
-            canvas_close[mode]()
+            const { width, actualBoundingBoxAscent, actualBoundingBoxDescent } = engine.canvas_ctx.measureText(text || x)
+            return [width, actualBoundingBoxAscent + actualBoundingBoxDescent]
+        },
+        native_dict_poly: {
+            poly2: (mode, verts, x, y, scale = 1, angle = 0, ox = 0, oy = 0) => {
+                let index = 0
+                engine.canvas_ctx.beginPath()
+                while (index < verts.length) {
+                    const px = verts[index];
+                    const py = verts[index + 1];
+                    const xx = x + ((ox - px) * -scale * Math.cos(angle)) - ((oy - py) * -scale * Math.sin(angle));
+                    const yy = y + ((oy - px) * -scale * Math.sin(angle)) + ((ox - py) * -scale * Math.cos(angle));
+                    if (index < 2) {
+                        engine.canvas_ctx.moveTo(xx, yy)
+                    } else {
+                        engine.canvas_ctx.lineTo(xx, yy)
+                    }
+                    index += 2;
+                }
+                engine.canvas_close[mode]()
+            }
+        },
+        native_dict_http: {
+            handler: (self) => {
+                const method = self.method
+                const headers = new Headers(self.headers_dict)
+                const params = new URLSearchParams(self.params_dict)
+                const url = `${self.url}` + '?' + params.toString()
+                const body = ['HEAD', 'GET'].includes(method) ? null : self.body_content
+                self.promise()
+                fetch(url, {
+                    body: body,
+                    method: method,
+                    headers: headers
+                })
+                .then((response) => {
+                    self.set('ok', response.ok)
+                    self.set('status', response.status)
+                    return response.text()
+                })
+                .then((content) => {
+                    self.set('body', content)
+                    self.resolve()
+                })
+                .catch((error) => {
+                    self.set('ok', false)
+                    self.set('error', `${error}`)
+                    self.resolve()
+                })
+            }
+        },
+        native_dict_json: {
+            encode: JSON.stringify,
+            decode: JSON.parse
         }
-    })
-    lua.global.set('native_dict_http', {
-        handler: (self) => {
-           const method = self.method
-           const headers = new Headers(self.headers_dict)
-           const params = new URLSearchParams(self.params_dict)
-           const url = `${self.url}` + '?' + params.toString()
-           const body = ['HEAD', 'GET'].includes(method)? null: self.body_content
-           self.promise()
-           fetch(url, {
-                body: body,
-                method: method,
-                headers: headers
-            })
-            .then((response) => {
-                self.set('ok', response.ok)
-                self.set('status', response.status)
-                return response.text()
-            })
-            .then((content) => {
-                self.set('body', content)
-                self.resolve()
-            })
-            .catch((error) => {
-                self.set('ok', false)
-                self.set('error', `${error}`)
-                self.resolve()
-            })
-        }
-    })
-    lua.global.set('native_dict_json', {
-        encode: JSON.stringify,
-        decode: JSON.parse
-    })
+    }    
+}
 
-    if (body_element.clientWidth > body_element.clientHeight) {
-        canvas_element.height = body_element.clientHeight
-        canvas_element.width = body_element.clientWidth
+function errorController(func) {
+    if (engine.stop) {
+        return
+    }
+    if (!engine.error.capture) {
+        return func()
+    }
+    try {
+        func()
+    }
+    catch(e) {
+        if (engine.error.console) {
+            console.log(e)
+        }
+        if (engine.error.stop) {
+            engine.stop = true
+        }
+        if (engine.error.canvas) {
+            engine.global.native_draw_start()
+            engine.global.native_draw_clear(0x0000FFFF)
+            engine.global.native_draw_color(0xFFFFFFFF)
+            engine.global.native_draw_text(8, 16, 'Fatal ERROR')
+            engine.global.native_draw_text(8, 32, e.toString())
+        }
+        if (engine.error.callback) {
+            engine.error.callback(e)
+        }
+    }
+}
+
+function resizeCanvas() {
+    if (engine.body_element.clientWidth > engine.body_element.clientHeight) {
+        engine.canvas_element.height = engine.body_element.clientHeight
+        engine.canvas_element.width = engine.body_element.clientWidth
     }
     else {
-        canvas_element.height = Math.floor(body_element.clientHeight / 2)
-        canvas_element.width = body_element.clientWidth
+        engine.canvas_element.height = Math.floor(engine.body_element.clientHeight / 2)
+        engine.canvas_element.width = engine.body_element.clientWidth
     }
+}
 
-    await lua.doString(engine_lua)
-    const engine_callbacks = {
-        init: lua.global.get('native_callback_init'),
-        update: lua.global.get('native_callback_loop'),
-        draw: lua.global.get('native_callback_draw'),
-        keyboard: lua.global.get('native_callback_keyboard'),
+const gly = {
+    init: (canvas_selector) => {
+        engine.body_element = document.querySelector('body')
+        engine.canvas_element = document.querySelector(canvas_selector)
+        engine.canvas_ctx = engine.canvas_element.getContext("2d")
+        engine.canvas_close = [
+            () => engine.canvas_ctx.fill(),
+            () => {
+                engine.canvas_ctx.closePath()
+                engine.canvas_ctx.stroke()
+            },
+            () => engine.canvas_ctx.stroke()
+        ]
+        resizeCanvas()
+    },
+    load: (game_file) => {
+        const {width, height} = engine.canvas_element
+        errorController(() => {
+            engine.listen.native_callback_init(width, height, game_file)
+        })  
+    },
+    input: (key, value) => {
+        errorController(() => {
+            engine.listen.native_callback_keyboard(key, value)
+        })
+    },
+    error: (behavior, error_callback) => {
+        const silent = behavior.includes('silent')
+        engine.error.capture = !behavior.includes('default')
+        engine.error.console = !silent && behavior.includes('console')
+        engine.error.canvas = !silent && behavior.includes('canvas')
+        engine.error.stop = !silent && behavior.includes('stop')
+        engine.error.callback = error_callback
+    },
+    resize: () => {
+        resizeCanvas()
+        const {width, height} = engine.canvas_element
+        errorController(() => {
+            engine.listen.native_callback_resize(width, height)
+        })
+    },
+    update: (milis) => {
+        errorController(() => {
+            engine.listen.native_callback_loop(milis)
+            engine.listen.native_callback_draw()
+        })
+    },
+    global: {
+        set: (var_name, value) => {
+            engine.listen[var_name] = value
+        },
+        get: (var_name) => {
+            return engine.global[var_name]
+        }
     }
-    engine_callbacks.init(canvas_element.width, canvas_element.height, game_lua)
+}
 
-    setTimeout(() => {
-        const keys = [
-            [403, 'red'],
-            [404, 'green'],
-            [405, 'yellow'],
-            [406, 'blue'],
-            ['KeyZ', 'red'],
-            ['KeyX', 'green'],
-            ['KeyC', 'yellow'],
-            ['KeyV', 'blue'],
-            ['Enter', 'enter'],
-            ['ArrowUp', 'up'],
-            ['ArrowDown', 'down'],
-            ['ArrowLeft', 'left'],
-            ['ArrowRight', 'right'],
-        ];
-    
-        const keyHandler = (ev) => keys
-            .filter(key => [ev.code, ev.keyCode].includes(key[0]))
-            .map(key => engine_callbacks.keyboard(key[1], Number(ev.type === 'keydown')))
-
-        window.addEventListener('keydown', keyHandler)
-        window.addEventListener('keyup', keyHandler)
-    }, 100)
-       
-    const tick = () => {
-        const now = new Date()
-        const milis = now.getTime()
-        engine_callbacks.update(milis)
-        engine_callbacks.draw()
-        window.requestAnimationFrame(tick)
-    }
-
-    tick()
-})
+if (typeof exports === 'object' && typeof module !== 'undefined') {
+    module.exports = gly;
+} else if (typeof define === 'function' && define.amd) {
+    define([], function() {
+        return gly;
+    });
+} else {
+    window.gly = gly;
+}
