58eee62e6ccecb4c41ca22c96d96004a596999cc
RodrigoDornelles <rodrigo@dornelles.me>
2024-10-07 16:45:39 -0300

    docs: balance pong game #57

--- examples/pong/game.lua
+++ examples/pong/game.lua
@@ -1,20 +1,21 @@
 local function init(std, game)
     game.highscore = game.highscore or 0
-    game.player_size = game.height/8
-    game.player_pos = game.height/2 - game.player_size/2
+    game.player_pos = game.height/2
     game.ball_pos_x = game.width/2
     game.ball_pos_y = game.height/2
-    game.ball_spd_x = 0.3
-    game.ball_spd_y = 0.06
-    game.ball_size = 8
+    game.ball_spd_x = 500
+    game.ball_spd_y = 300
     game.score = 0
 end
 
 local function loop(std, game)
     -- moves
-    game.player_pos = std.math.clamp(game.player_pos + (std.key.axis.y * 7), 0, game.height - game.player_size)
-    game.ball_pos_x = game.ball_pos_x + (game.ball_spd_x * game.dt)
-    game.ball_pos_y = game.ball_pos_y + (game.ball_spd_y * game.dt)
+    game.ball_size = std.math.max(game.width, game.height) / 160
+    game.player_size = std.math.min(game.width, game.height) / 8
+    game.ball_pos_x = game.ball_pos_x + (game.width * game.ball_spd_x * game.dt)/1000000
+    game.ball_pos_y = game.ball_pos_y + (game.height * game.ball_spd_y * game.dt)/1000000
+    game.player_pos = std.math.clamp(game.player_pos + (std.key.axis.y * game.ball_size), 0, game.height - game.player_size)  
+
     -- colisions
     if game.ball_pos_x >= (game.width - game.ball_size) then
         game.ball_spd_x = -std.math.abs(game.ball_spd_x)
@@ -27,10 +28,8 @@ local function loop(std, game)
     end
     if game.ball_pos_x <= 0 then 
         if std.math.clamp(game.ball_pos_y, game.player_pos, game.player_pos + game.player_size) == game.ball_pos_y then
-            local new_spd_y = std.math.clamp(game.ball_spd_y + (game.player_pos % 10) - 5, -10, 10)
-            game.ball_spd_y = game.ball_spd_y == 0 and new_spd_y == 0 and 20 or new_spd_y -- WOW!
-            game.ball_spd_y = game.ball_spd_y / 16
-            game.ball_spd_x = std.math.abs(game.ball_spd_x) * 1.003
+            game.ball_spd_y = game.ball_spd_y + 500 - (game.milis % 1000)
+            game.ball_spd_x = std.math.abs(game.ball_spd_x) * 1.1
             game.score = game.score + 1
         else
             std.game.reset()
@@ -41,11 +40,11 @@ end
 local function draw(std, game)
     std.draw.clear(std.color.black)
     std.draw.color(std.color.white)
-    std.draw.rect(0, 4, game.player_pos, 8, game.player_size)
+    std.draw.rect(0, game.ball_size, game.player_pos, game.ball_size, game.player_size)
     std.draw.rect(0, game.ball_pos_x, game.ball_pos_y, game.ball_size, game.ball_size)
-    std.draw.font('Tiresias', 32)
-    std.draw.text(game.width/4, 16, game.score)
-    std.draw.text(game.width/4 * 3, 16, game.highscore)
+    std.draw.font(game.ball_size * 8)
+    std.draw.text(game.width/4, game.ball_size, game.score)
+    std.draw.text(game.width/4 * 3, game.ball_size, game.highscore)
 end
 
 local function exit(std, game)
