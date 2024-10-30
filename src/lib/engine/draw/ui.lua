2c4544e182faa4d8e1115196c6ced6675f38e3af
RodrigoDornelles <rodrigo@dornelles.me>
2024-10-30 14:35:59 -0300

    feat: grid resize and docs

--- src/lib/engine/draw/ui.lua
+++ src/lib/engine/draw/ui.lua
@@ -1,9 +1,105 @@
 local math = require('math')
 local util_decorator = require('src/lib/util/decorator')
 
+--! @defgroup std
+--! @{
+--! @defgroup ui
+--! @{
+--!
+--! @details
+--! @page ui_grid Grid System
+--!
+--! The grid system is very versatile, and adjusts itself with the resolution and can be used by nesting one grid inside another,
+--! the best of all is that in frameworks that limit you to thinking in 12 or 24 columns like
+--! [bootstrap](https://getbootstrap.com/docs/5.0/layout/grid/)
+--! you must define how many columns yourself.
+--!
+--! @par Example
+--! @code{.java}
+--! local function init(std, data)
+--!     std.ui.grid('4x8 full6x8 ultra8x8')
+--!         :add_items(list_of_widgets)
+--! end
+--! @endcode
+--!
+--! @par Breakpoints
+--! @todo comming soon
+--!
+--! |        | 1seg | SD 480 | HD 720  | FULL HD 1080 | QUAD HD 1440 | ULTRA HD 4K |
+--! | :----- |      | :----: | :-----: | :----------: | :----------: | :---------: |
+--! | prefix |      | sd     | hd      | full         | quad         | ultra       |
+--! | width  | >0px | >679px | >1279px | >1919px      | >2569px      | >3839px     |
+--! 
+--! @par Offset
+--! To create blank columns, simply add an empty table @c {} to represent an empty node.
+--! You can also specify the size of these whitespace columns as needed.
+--! @startsalt
+--! {+
+--!   . | . | [btn0] 
+--!   [btn1] | [btn2] | [btn3]
+--! }
+--! @endsalt
+--! @code{.java}
+--! std.ui.grid('3x2')
+--!     :add({}, 2)
+--!     :add(btn0)
+--!     :add(btn1)
+--!     :add(btn2)
+--!     :add(btn3)
+--! @endcode
+--!
+--! @par Columns
+--!
+--! You can add several different items to your grid: classes, nodes, offsets, entire applications and even another grid.
+--!
+--! @li @b class
+--! @code{.java}
+--! local btn = {
+--!     draw=function(std, data)end
+--! }
+--! std.ui.grid('1x1'):add(btn)
+--! @endcode
+--!
+--! @li @b node
+--! @code{.java}
+--! local btn_node = std.node.load(btn)
+--! std.ui.grid('1x1'):add(node_btn)
+--! @endcode
+--!
+--! @li @b offset
+--! @code{.java}
+--! std.ui.grid('1x1'):add({})
+--! @endcode
+--!
+--! @li @b application
+--! @code{.java}
+--! local game = {
+--!     meta={
+--!        title='pong'
+--!     },
+--!     callbacks={
+--!         init=function() end,
+--!         loop=function() end,
+--!         draw=function() end,
+--!         exit=function() end
+--!     }
+--! }
+--! std.ui.grid('1x1'):add(game)
+--! @endcode
+--! 
+--! @li @b grid
+--! @code{.java}
+--! std.ui.grid('1x1')
+--!      :add(std.ui.grid('1x1')
+--!         :add(btn)
+--!      )
+--! @endcode
+
 --! @hideparam std
 --! @hideparam engine
 --! @hideparam self
+--! @param [in,out] application new column
+--! @param [in] size column width in blocks
 local function add(std, engine, self, application, size)
     local index = #self.items_node + 1
     local node = application.node or std.node.load(application.node or application)
@@ -23,11 +119,27 @@ local function add(std, engine, self, application, size)
     return self
 end
 
+--! @hideparam std
+--! @hideparam engine
+--! @hideparam self
+--! @param [in,out] list of application columns
+local function add_items(std, engine, self, applications)
+    local index = 1
+    while application and index < #application do
+        add(std, engine, self, applications[index])
+    end
+    return self
+end
+
+--! @hideparam self
+--! @param [in] id item index
+--! @return node
 local function get_item(self, id)
     return self.items_node[id]
 end
 
---! hideparam self
+--! @cond
+--! @todo better name
 local function update_positions(self)
     local index = 1
     local x, y = 0, 0
@@ -59,9 +171,10 @@ local function update_positions(self)
 
     return self
 end
+--! @endcond
 
---! @hideparam std
---! @hideparam engine
+--! @}
+--! @}
 local function grid(std, engine, layout)
     local rows, cols = layout:match('(%d+)x(%d+)')
     local node = std.node.load({})
@@ -74,9 +187,22 @@ local function grid(std, engine, layout)
         items_ui = {},
         node=node,
         add=util_decorator.prefix2(std, engine, add),
+        add_items=util_decorator.prefix2(std, engine, add)
         update_positions=update_positions,
         get_item=get_item
     }
+
+    if engine.root == engine.current then
+        node.callbacks.resize = function()
+            if node.config.parent ~= engine.root then
+                node.callbacks.resize = nil
+                return
+            end
+            node.data.width = engine.root.data.width
+            node.data.height = engine.root.data.height
+            grid_system:update_positions()
+        end
+    end
 
     std.node.spawn(node)
     return grid_system
