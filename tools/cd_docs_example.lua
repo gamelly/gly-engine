40356cbc6fe069ce1d28874c2511dd1dd40f1b3e
RodrigoDornelles <rodrigo@dornelles.me>
2024-11-01 08:52:15 -0300

    ci: fix

--- tools/cd_docs_example.lua
+++ tools/cd_docs_example.lua
@@ -11,5 +11,5 @@ cmd('./cli.sh build @'..game..' --core html5 --dist ./html/')
 if game == 'dvdplayer' then
     cmd('cp assets/icon80x80.png ./html/icon80x80.png')
 elseif game == 'launcher' then
-    cmd('./cli.sh fs-replace ./html/game.lua ./html/game.lua --format http --replace https')
+    cmd('./cli.sh fs-replace ./html/game.lua ./html/game.lua --format http: --replace https:')
 end
