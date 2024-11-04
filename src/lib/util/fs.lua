a8352d4a5504d98c6efd45e7534082f6dc036e2f
RodrigoDornelles <rodrigo@dornelles.me>
2024-11-04 12:02:12 -0300

    feat: create util_fs

---
+++ src/lib/util/fs.lua
@@ -0,0 +1,99 @@
+local function get_ext(self)
+    return function()
+        return self.extension
+    end
+end
+
+local function get_file(self)
+    return function()
+        if self.extension and #self.extension > 0 then
+            return self.filename..'.'..self.extension
+        end
+        return self.filename
+    end
+end
+
+local function get_filename(self)
+    return function()
+        return self.filename
+    end
+end
+
+local function get_path(self, separator)
+    return function()
+        local index = 1
+        local content = self.absolute and separator or ''
+        if self.absolute and separator == '\\' then
+            content = self.windriver..':'..content
+        end
+        while index <= #self.path do
+            content = content..self.path[index]..separator
+            index = index + 1
+        end
+        return content
+    end
+end
+
+local function get_fullfilepath(self, separator)
+    return function()
+        return get_path(self, separator)()..get_file(self)()
+    end
+end
+
+local function scan(type_file)
+    return function(src)
+        src = (src or ''):gsub('%s*$', '')
+        if #src == 0 then return nil end
+
+        local firstchar = src:sub(1,1)
+        local windriver = string.match(src, '^([A-Z]):[/\\]')
+        local separator = (mock_separator or (_G.package and _G.package.config) or '/'):sub(1,1)
+
+        local self = {
+            windriver= 'WINDOWS',
+            absolute = false,
+            extension='',
+            filename='',
+            path={}
+        }
+
+        if firstchar == '/' or firstchar == '\\' or windriver then
+            self.windriver = windriver or 'C'
+            self.absolute = true
+        else
+            src = '.'..separator..src
+        end
+
+        if windriver then
+            src = src:sub(3)
+        end
+
+        src:gsub("([^/\\]+)", function(part)
+            self.path[#self.path + 1] = part
+        end)
+
+        if type_file then
+            self.filename, self.extension = self.path[#self.path]:match("^(.-)%.([^%.]+)$")
+            self.filename = self.filename or self.path[#self.path]
+            self.extension = self.extension or ''
+            self.path[#self.path] = nil
+        end
+
+        return {
+            get_ext=get_ext(self),
+            get_file=get_file(self),
+            get_filename=get_filename(self),
+            get_sys_path=get_path(self, separator),
+            get_win_path=get_path(self, '\\'),
+            get_unix_path=get_path(self, '/'),
+            get_fullfilepath=get_fullfilepath(self, separator)
+        }
+    end
+end
+
+local P = {
+    file = scan(true),
+    path = scan(false)
+}
+
+return P
---
+++ tests/test_lib_util_fs.lua
@@ -0,0 +1,83 @@
+local luaunit = require('luaunit')
+local util_fs = require('src/lib/util/fs')
+
+function test_unix_foo_bar_z_txt()
+    local file = util_fs.file('foo/bar/z.txt')
+    luaunit.assertEquals(file.get_file(), 'z.txt')
+    luaunit.assertEquals(file.get_filename(), 'z')
+    luaunit.assertEquals(file.get_ext(), 'txt')
+    luaunit.assertEquals(file.get_win_path(), '.\\foo\\bar\\')
+    luaunit.assertEquals(file.get_unix_path(), './foo/bar/')
+end
+
+function test_win_baz_bar_y_txt()
+    local file = util_fs.file('baz\\bar\\y.exe')
+    luaunit.assertEquals(file.get_file(), 'y.exe')
+    luaunit.assertEquals(file.get_filename(), 'y')
+    luaunit.assertEquals(file.get_ext(), 'exe')
+    luaunit.assertEquals(file.get_win_path(), '.\\baz\\bar\\')
+    luaunit.assertEquals(file.get_unix_path(), './baz/bar/')
+end
+
+function test_unix_absolute()
+    local file = util_fs.file('/etc/hosts')
+    luaunit.assertEquals(file.get_file(), 'hosts')
+    luaunit.assertEquals(file.get_filename(), 'hosts')
+    luaunit.assertEquals(file.get_ext(), '')
+    luaunit.assertEquals(file.get_unix_path(), '/etc/')
+    luaunit.assertEquals(file.get_win_path(), 'C:\\etc\\')
+end
+
+function test_win_absolute()
+    local file = util_fs.file('\\Windows\\System32\\drivers\\etc\\hosts')
+    luaunit.assertEquals(file.get_file(), 'hosts')
+    luaunit.assertEquals(file.get_filename(), 'hosts')
+    luaunit.assertEquals(file.get_ext(), '')
+    luaunit.assertEquals(file.get_win_path(), 'C:\\Windows\\System32\\drivers\\etc\\')
+    luaunit.assertEquals(file.get_unix_path(), '/Windows/System32/drivers/etc/')
+end
+
+function test_win_absolute_with_driver()
+    local file = util_fs.file('D:\\Windows\\System32\\drivers\\etc\\hosts')
+    luaunit.assertEquals(file.get_file(), 'hosts')
+    luaunit.assertEquals(file.get_filename(), 'hosts')
+    luaunit.assertEquals(file.get_ext(), '')
+    luaunit.assertEquals(file.get_win_path(), 'D:\\Windows\\System32\\drivers\\etc\\')
+    luaunit.assertEquals(file.get_unix_path(), '/Windows/System32/drivers/etc/')
+end
+
+function test_unix_path()
+    local file = util_fs.path('/etc/bin')
+    luaunit.assertEquals(file.get_file(), '')
+    luaunit.assertEquals(file.get_filename(), '')
+    luaunit.assertEquals(file.get_ext(), '')
+    luaunit.assertEquals(file.get_win_path(), 'C:\\etc\\bin\\')
+    luaunit.assertEquals(file.get_unix_path(), '/etc/bin/')
+end
+
+function test_win_path()
+    local file = util_fs.path('C:/win32/program files')
+    luaunit.assertEquals(file.get_file(), '')
+    luaunit.assertEquals(file.get_filename(), '')
+    luaunit.assertEquals(file.get_ext(), '')
+    luaunit.assertEquals(file.get_win_path(), 'C:\\win32\\program files\\')
+    luaunit.assertEquals(file.get_unix_path(), '/win32/program files/')
+end
+
+function test_detect_separator()
+    mock_separator = '\\'
+    local file_win = util_fs.path('/user')
+    mock_separator = '/'
+    local file_unix = util_fs.path('/home')
+    luaunit.assertEquals(file_win.get_fullfilepath(), 'C:\\user\\')
+    luaunit.assertEquals(file_unix.get_fullfilepath(), '/home/')
+end
+
+function test_empty_protect()
+    luaunit.assertEquals(util_fs.file(nil), nil)
+    luaunit.assertEquals(util_fs.file(''), nil)
+    luaunit.assertEquals(util_fs.file(' '), nil)
+    luaunit.assertEquals(util_fs.file('\n'), nil)
+end
+
+os.exit(luaunit.LuaUnit.run())
