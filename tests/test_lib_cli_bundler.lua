2d0a16726e9c2f41be439b2b7206c3ad042d1a89
RodrigoDornelles <rodrigo@dornelles.me>
2025-01-06 15:52:26 -0300

    tests: recursion in bundler

--- tests/test_lib_cli_bundler.lua
+++ tests/test_lib_cli_bundler.lua
@@ -3,6 +3,10 @@ local zeebo_bundler = require('src/lib/cli/bundler')
 local mock_io = require('mock/io')
 
 io.open = mock_io.open({
+    ['src/foo.lua'] = 'local math = require(\'math\')',
+    ['src/bar.lua'] = 'local z = require(\'foo\')',
+    ['src/baz.lua'] = 'local z = require(\'bar\')',
+    ['src/biz.lua'] = 'local z = require(\'baz\')',
     ['src/lib_common_math.lua'] = 'local function sum(a, b)\n'
         ..' return a + b\n'
         ..'end\n'
@@ -15,12 +19,19 @@ io.open = mock_io.open({
         ..'return zeebo_math.sum(1, 2)\n'
 })
 
-function test_simple_bundler()
+function test_sample()
     zeebo_bundler.build('src/', 'main.lua', 'dist/main1.lua')
     local dist_file = io.open('dist/main1.lua', 'r')
     local dist_text = dist_file and dist_file:read('*a')
     local dist_func = loadstring and loadstring(dist_text) or load(dist_text)
-    luaunit.assertEquals(dist_func(), 3)
+    assert(dist_func() == 3)
+end
+
+function test_recursion()
+    zeebo_bundler.build('src/', 'biz.lua', 'dist/main2.lua')
+    local dist_file = io.open('dist/main2.lua', 'r')
+    local dist_text = dist_file and dist_file:read('*a')
+    assert(dist_text:match('_G.math'))
 end
 
 os.exit(luaunit.LuaUnit.run())
