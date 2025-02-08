a98c156362a6eb3a6ffe184b045309176f1947b4
Finalshare90 <isaacpmartins08@gmail.com>
2025-02-08 17:09:42 -0300

    Dropped luaunit for shared_args tests

--- tests/test_shared_args.lua
+++ tests/test_shared_args.lua
@@ -1,64 +1,66 @@
+local test = require('src/lib/util/test')
 local luaunit = require('luaunit')
 local zeebo_args = require('src/lib/common/args')
 
 function test_shared_args_get_basic()
     local args = {'--option', 'value'}
-    luaunit.assertEquals(zeebo_args.get(args, 'option', 'default'), 'value')
+    assert(zeebo_args.get(args, 'option', 'default') == 'value')
 end
 
 function test_shared_args_get_default_value()
     local args = {'--other', 'value'}
-    luaunit.assertEquals(zeebo_args.get(args, 'option', 'default'), 'default')
+    assert(zeebo_args.get(args, 'option', 'default') == 'default')
 end
 
 function test_shared_args_get_missing_argument()
     local args = {'--option'}
-    luaunit.assertEquals(zeebo_args.get(args, 'option', 'default'), 'default')
+    assert(zeebo_args.get(args, 'option', 'default') == 'default')
 end
 
 function test_shared_args_get_no_arguments()
     local args = {}
-    luaunit.assertEquals(zeebo_args.get(args, 'option', 'default'), 'default')
+    assert(zeebo_args.get(args, 'option', 'default') == 'default')
 end
 
 function test_shared_args_has_true()
     local args = {'--option'}
-    luaunit.assertTrue(zeebo_args.has(args, 'option'))
+    assert(zeebo_args.has(args, 'option') == true)
 end
 
 function test_shared_args_has_false()
     local args = {'--other'}
-    luaunit.assertFalse(zeebo_args.has(args, 'option'))
+    assert(zeebo_args.has(args, 'option') == false)
 end
 
 function test_shared_args_has_no_arguments()
     local args = {}
-    luaunit.assertFalse(zeebo_args.has(args, 'option'))
+    assert(zeebo_args.has(args, 'option') == false)
 end
 
 function test_shared_args_param_basic()
     local args = {'value1', '--flag', 'param1', '--flag2', 'param2', 'value2'}
     local args_get = {'flag', 'flag2'}
-    luaunit.assertEquals(zeebo_args.param(args, args_get, 1, 'default'), 'value1')
-    luaunit.assertEquals(zeebo_args.param(args, args_get, 2, 'default'), 'value2')
+    assert(zeebo_args.param(args, args_get, 1, 'default') == 'value1')
+    assert(zeebo_args.param(args, args_get, 2, 'default') == 'value2')
 end
 
 function test_shared_args_param_position_out_of_range()
     local args = {'value1', '--flag', 'param1'}
     local args_get = {'flag'}
-    luaunit.assertEquals(zeebo_args.param(args, args_get, 2, 'default'), 'default')
+    assert(zeebo_args.param(args, args_get, 2, 'default') == 'default')
 end
 
 function test_shared_args_param_no_arguments()
     local args = {}
     local args_get = {'flag'}
-    luaunit.assertEquals(zeebo_args.param(args, args_get, 1, 'default'), 'default')
+    assert(zeebo_args.param(args, args_get, 1, 'default') == 'default')
 end
 
 function test_shared_args_param_single_argument()
     local args = {'value1'}
     local args_get = {}
-    luaunit.assertEquals(zeebo_args.param(args, args_get, 1, 'default'), 'value1')
+    assert(zeebo_args.param(args, args_get, 1, 'default') == 'value1')
 end
 
-os.exit(luaunit.LuaUnit.run())
+test.unit(_G)
+--os.exit(luaunit.LuaUnit.run())
