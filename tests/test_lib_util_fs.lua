0ad5fa7632a75f996672a4a3af0452237dae50fa
Finalshare90 <isaacpmartins08@gmail.com>
2025-02-07 21:47:05 -0300

    dropped luaunit on fs tests

--- tests/test_lib_util_fs.lua
+++ tests/test_lib_util_fs.lua
@@ -1,76 +1,104 @@
-local luaunit = require('luaunit')
 local util_fs = require('src/lib/util/fs')
+local test = require('src/lib/util/test')
+
 
 function test_path_with_src2()
-    local path = util_fs.path('foo/bar', 'extra.txt')
-    luaunit.assertEquals(path.get_file(), 'extra.txt')
-    luaunit.assertEquals(path.get_filename(), 'extra')
-    luaunit.assertEquals(path.get_ext(), 'txt')
-    luaunit.assertEquals(path.get_win_path(), '.\\foo\\bar\\')
-    luaunit.assertEquals(path.get_unix_path(), './foo/bar/')
+	local path = util_fs.path('foo/bar', 'extra.txt')
+
+	print("test_path_with_src2\n")
+	
+	assert(path.get_file() == 'extra.txt')
+	assert(path.get_filename() == 'extra')
+	assert(path.get_ext() == 'txt')
+	assert(path.get_win_path() == '.\\foo\\bar\\')
+	assert(path.get_unix_path() == './foo/bar/')
 end
+
 
 function test_unix_foo_bar_z_txt()
     local file = util_fs.file('foo/bar/z.txt')
-    luaunit.assertEquals(file.get_file(), 'z.txt')
-    luaunit.assertEquals(file.get_filename(), 'z')
-    luaunit.assertEquals(file.get_ext(), 'txt')
-    luaunit.assertEquals(file.get_win_path(), '.\\foo\\bar\\')
-    luaunit.assertEquals(file.get_unix_path(), './foo/bar/')
+
+	print("test_unix_foo_bar_z_txt\n")
+	
+	assert(file.get_file() == 'z.txt')
+	assert(file.get_filename() == 'z')
+    assert(file.get_ext() == 'txt')
+    assert(file.get_win_path() == '.\\foo\\bar\\')
+    assert(file.get_unix_path() == './foo/bar/')
 end
 
 function test_win_baz_bar_y_txt()
     local file = util_fs.file('baz\\bar\\y.exe')
-    luaunit.assertEquals(file.get_file(), 'y.exe')
-    luaunit.assertEquals(file.get_filename(), 'y')
-    luaunit.assertEquals(file.get_ext(), 'exe')
-    luaunit.assertEquals(file.get_win_path(), '.\\baz\\bar\\')
-    luaunit.assertEquals(file.get_unix_path(), './baz/bar/')
+
+	print("test_win_baz_bar_y_txt\n")
+
+	assert(file.get_file() == 'y.exe')
+    assert(file.get_filename() == 'y')
+    assert(file.get_ext() == 'exe')
+    assert(file.get_win_path() == '.\\baz\\bar\\')
+    assert(file.get_unix_path() == './baz/bar/')
 end
 
+
 function test_unix_absolute()
-    local file = util_fs.file('/etc/hosts')
-    luaunit.assertEquals(file.get_file(), 'hosts')
-    luaunit.assertEquals(file.get_filename(), 'hosts')
-    luaunit.assertEquals(file.get_ext(), '')
-    luaunit.assertEquals(file.get_unix_path(), '/etc/')
-    luaunit.assertEquals(file.get_win_path(), 'C:\\etc\\')
+	local file = util_fs.file('/etc/hosts')
+	
+	print("test_unix_absolute\n")
+	
+    assert(file.get_file() == 'hosts')
+    assert(file.get_filename() == 'hosts')
+    assert(file.get_ext() == '')
+    assert(file.get_unix_path() == '/etc/')
+    assert(file.get_win_path() == 'C:\\etc\\')
 end
+
 
 function test_win_absolute()
     local file = util_fs.file('\\Windows\\System32\\drivers\\etc\\hosts')
-    luaunit.assertEquals(file.get_file(), 'hosts')
-    luaunit.assertEquals(file.get_filename(), 'hosts')
-    luaunit.assertEquals(file.get_ext(), '')
-    luaunit.assertEquals(file.get_win_path(), 'C:\\Windows\\System32\\drivers\\etc\\')
-    luaunit.assertEquals(file.get_unix_path(), '/Windows/System32/drivers/etc/')
+
+	print("test_win_absolute\n")
+	
+	assert(file.get_file() == 'hosts')
+    assert(file.get_filename() == 'hosts')
+    assert(file.get_ext() == '')
+    assert(file.get_win_path() == 'C:\\Windows\\System32\\drivers\\etc\\')
+    assert(file.get_unix_path() == '/Windows/System32/drivers/etc/')
 end
 
 function test_win_absolute_with_driver()
     local file = util_fs.file('D:\\Windows\\System32\\drivers\\etc\\hosts')
-    luaunit.assertEquals(file.get_file(), 'hosts')
-    luaunit.assertEquals(file.get_filename(), 'hosts')
-    luaunit.assertEquals(file.get_ext(), '')
-    luaunit.assertEquals(file.get_win_path(), 'D:\\Windows\\System32\\drivers\\etc\\')
-    luaunit.assertEquals(file.get_unix_path(), '/Windows/System32/drivers/etc/')
+
+	print("test_win_absolute_with_driver\n")
+	
+	assert(file.get_file() == 'hosts')
+    assert(file.get_filename() == 'hosts')
+    assert(file.get_ext() == '')
+    assert(file.get_win_path() == 'D:\\Windows\\System32\\drivers\\etc\\')
+    assert(file.get_unix_path() == '/Windows/System32/drivers/etc/')
 end
 
 function test_unix_path()
     local file = util_fs.path('/etc/bin')
-    luaunit.assertEquals(file.get_file(), '')
-    luaunit.assertEquals(file.get_filename(), '')
-    luaunit.assertEquals(file.get_ext(), '')
-    luaunit.assertEquals(file.get_win_path(), 'C:\\etc\\bin\\')
-    luaunit.assertEquals(file.get_unix_path(), '/etc/bin/')
+
+	print("test_unix_path\n")
+
+	assert(file.get_file() == '')
+    assert(file.get_filename() == '')
+    assert(file.get_ext() == '')
+    assert(file.get_win_path() == 'C:\\etc\\bin\\')
+    assert(file.get_unix_path() == '/etc/bin/')
 end
 
 function test_win_path()
     local file = util_fs.path('C:/win32/program files')
-    luaunit.assertEquals(file.get_file(), '')
-    luaunit.assertEquals(file.get_filename(), '')
-    luaunit.assertEquals(file.get_ext(), '')
-    luaunit.assertEquals(file.get_win_path(), 'C:\\win32\\program files\\')
-    luaunit.assertEquals(file.get_unix_path(), '/win32/program files/')
+
+	print("test_win_path\n")
+	
+	assert(file.get_file() == '')
+    assert(file.get_filename() == '')
+    assert(file.get_ext() == '')
+    assert(file.get_win_path() == 'C:\\win32\\program files\\')
+    assert(file.get_unix_path() == '/win32/program files/')
 end
 
 function test_detect_separator()
@@ -78,15 +106,20 @@ function test_detect_separator()
     local file_win = util_fs.path('/user')
     mock_separator = '/'
     local file_unix = util_fs.path('/home')
-    luaunit.assertEquals(file_win.get_fullfilepath(), 'C:\\user\\')
-    luaunit.assertEquals(file_unix.get_fullfilepath(), '/home/')
+
+	print("test_win_path\n")
+	
+    assert(file_win.get_fullfilepath() == 'C:\\user\\')
+    assert(file_unix.get_fullfilepath() == '/home/')
+
 end
 
 function test_empty_protect()
-    luaunit.assertEquals(util_fs.file(nil), nil)
-    luaunit.assertEquals(util_fs.file(''), nil)
-    luaunit.assertEquals(util_fs.file(' '), nil)
-    luaunit.assertEquals(util_fs.file('\n'), nil)
+	print("test_empty_protect\n")
+    assert(util_fs.file(nil) == nil)
+    assert(util_fs.file('') == nil)
+    assert(util_fs.file(' ') == nil)
+    assert(util_fs.file('\n') == nil)
 end
 
-os.exit(luaunit.LuaUnit.run())
+test.unit(_G)
