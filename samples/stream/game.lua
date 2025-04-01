0a605e7a92415c49e4b5b695f257ace37aa968da
RodrigoDornelles <rodrigo@dornelles.me>
2025-04-01 12:52:33 -0300

    feat: start sample stream

---
+++ samples/stream/game.lua
@@ -0,0 +1,88 @@
+local function init(std, data)
+    data.menu = 1
+    data.msg = 'loading...'
+    data.time = std.milis
+    data.wmax = 1
+    std.http.get('http://t.gamely.com.br/medias.json')
+        :error(function()
+            data.msg = std.http.error
+        end)
+        :failed(function()
+            data.msg = tostring(std.http.status)
+        end)
+        :success(function()
+            data.list = std.json.decode(std.http.body)
+            data.msg = nil
+        end)
+        :run()
+end
+
+local function loop(std, data)
+    if not data.list or #data.list == 0 then return end
+    if data.time + 300 < std.milis and std.key.press.any then
+        data.menu = std.math.clamp2(data.menu + std.key.axis.y, 1, #data.list)
+        data.time = std.milis
+        if std.key.press.a then
+            std.media.video():src(data.list[data.menu])
+        end
+        if std.key.press.b then
+            std.media.video():resume()
+        end
+        if std.key.press.c then
+            std.media.video():pause()
+        end
+        if std.key.press.d then
+            std.media.video():stop()
+        end
+        if std.key.press.left then
+            std.media.video():resize(640, 320)
+        end
+        if std.key.press.right then
+            std.media.video():resize(data.width, data.height)
+        end
+    end
+end
+
+local function draw(std, data)
+    if data.msg then
+        std.text.put(1, 1, data.msg)
+    end
+    if data.list and #data.list > 0 then
+        local w, h = data.width/6, data.height/4
+        local w2, h2, index = data.width - data.wmax - 16, h*2, 1
+        local h3 = (#data.list + 1) * 8
+        std.draw.color(std.color.blue)
+        std.draw.rect(0, w2, h, data.wmax + 16, h3)
+        std.draw.color(std.color.white)
+        std.draw.rect(1, w2, h, data.wmax + 16, h3)
+        std.text.font_size(8)
+        std.text.print(w2 + 4, (data.menu * 8) + h, '>', 1)
+        while index <= #data.list do
+            data.wmax = std.math.max(data.wmax, std.text.print_ex(data.width, (index * 8) + h, data.list[index], -1))
+            index = index + 1
+        end
+    end
+end
+
+local function exit(std, data)
+end
+
+local P = {
+    meta={
+        title='Media Streamming',
+        description='play videos',
+        author='Rodrigo Dornelles',
+        version='1.0.0'
+    },
+    config={
+        require='http json media.video'
+    },
+    callbacks={
+        init=init,
+        loop=loop,
+        draw=draw,
+        exit=exit
+    }
+}
+
+return P
