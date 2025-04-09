981668e1dfbb15195434454e4247247f31e9ef06
rodrigodornelles <rodrigo.dornelles.poa@gmail.com>
2025-04-09 18:30:56 -0300

    docs: organize stream demos

--- tools/cd_docs_example.lua
+++ tools/cd_docs_example.lua
@@ -13,12 +13,12 @@ elseif game == 'gridsystem' or game == 'maze3d' or game == 'two_games' then
 elseif game == 'pong' then
     cmd('./cli.sh build @'..game..' --core html5_micro --dist ./html/ --fengari --enginecdn')
 elseif game == 'fakestream' then
-    cmd('./cli.sh build @stream --core html5_lite --dist ./html/ --fengari --enginecdn --videofake')
+    cmd('./cli.sh build @stream --core html5 --dist ./html/ --enterprise --enginecdn --videofake')
 elseif game == 'rickstream' then
-    cmd('./cli.sh build @stream --core html5 --fengari --dist ./html/ --enginecdn --videojs --enterprise')
+    cmd('./cli.sh build @stream --core html5 --fengari --dist ./html/ --enginecdn')
     cmd('./cli.sh fs-replace ./html/game.lua ./html/game.lua --format medias --replace rick')
 elseif game == 'videostream' then
-    cmd('./cli.sh build @stream --core html5_ginga --dist ./html/ --fengari --enginecdn --videojs --enterprise')
+    cmd('./cli.sh build @stream --core html5 --dist ./html/ --fengari --enginecdn --videojs')
 else
     cmd('./cli.sh build @'..game..' --core html5_lite --dist ./html/ --fengari --enginecdn')
 end
